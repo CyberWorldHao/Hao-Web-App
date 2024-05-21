8a4e6aef81477989b241611b94e085b827410d73
CWH <ahaoahao2000@gmail.com>
2024-05-21 17:04:19 +0800

    Continue

--- src/components/navs&tabs.jsx
+++ src/components/navs&tabs.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 ["Home", "Menu", "Music", "Jobs"];
 
-function BasicExample() {
+function Navs() {
   return (
     <Nav
-      activeKey="/home"
+      justify
+      variant="tabs"
+      defaultActiveKey="/home"
       onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
     >
       <Nav.Item>
         <Nav.Link href="/home">Home</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/menu">Menu</Nav.Link>
+        <Nav.Link href="/menu">Menu</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/music">Music</Nav.Link>
+        <Nav.Link href="/music">Music</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="/jobs">Jobs</Nav.Link>
+        <Nav.Link href="/jobs">Jobs</Nav.Link>
+        <Nav.Link href="/phoneBook">Phone Book</Nav.Link>
         <Nav.Link eventKey="disabled" disabled>
           Try it On App
         </Nav.Link>
@@ -27,4 +30,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default Navs;
