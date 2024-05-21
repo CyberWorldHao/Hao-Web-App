f4491f0b3707ccdc1352c37067219d19c67d0c89
CWH <ahaoahao2000@gmail.com>
2024-05-21 16:17:44 +0800

    Pagination & bring back musicl.html

---
+++ src/components/pagination.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Pagination from "react-bootstrap/Pagination";
+
+let active = 1;
+let items = ["Home", "Menu", "Music", "Jobs"];
+
+let paginationItems = items.map((item, index) => (
+  <Pagination.Item key={index + 1} active={index + 1 === active}>
+    {item}
+  </Pagination.Item>
+));
+
+const PaginationBasic = () => {
+  return (
+    <div>
+      <Pagination>{paginationItems}</Pagination>
+    </div>
+  );
+};
+
+export default PaginationBasic;
