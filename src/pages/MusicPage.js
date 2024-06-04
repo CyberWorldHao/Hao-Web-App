1b2f5b397ece6b304746b05b8f0bac5b06a15f65
CWH <55231366+CyberWorldHao@users.noreply.github.com>
2024-06-04 14:08:07 +0800

    Make Music Page responsive

--- src/pages/MusicPage.js
+++ src/pages/MusicPage.js
@@ -27,7 +27,7 @@ function MusicPage() {
           <Navs />
           <div>
             <h2 className="mt-5">Music at La La Land</h2>
-            <p>
+            <p className="musicText">
               Step into the enchanting world of "La La Land," where dreams come
               alive against the vibrant backdrop of Los Angeles. This critically
               acclaimed musical masterpiece, directed by Damien Chazelle, weaves
@@ -41,71 +41,110 @@ function MusicPage() {
               that will leave you inspired and dreaming long after the final
               note fades.
             </p>
-            <h4 className="musicPageHeader4">City of Stars</h4>
-            <div className="soundTrackImg">
-              <a
-                href={"https://www.youtube.com/watch?v=GTWqwSNQCcg"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={CityOfStars}
-                  alt="City Of Stars"
-                  className="songImg"
-                />
-              </a>
-              {` Best Original Score, and Best Original Song`}
+            <div className="soundTrackGroup">
+              <div>
+                <h4 className="musicPageHeader4">City of Stars</h4>
+                <div className="soundTrackImg">
+                  <a
+                    href={"https://www.youtube.com/watch?v=GTWqwSNQCcg"}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={CityOfStars}
+                      alt="City Of Stars"
+                      className="songImg"
+                      align="left"
+                    />
+                  </a>
+                  <p>
+                    Their hopes, dreams, and the challenges they face in
+                    pursuing their ambitions in the bustling city of Los
+                    Angeles. Its haunting melody and introspective lyrics
+                    capture the essence of the characters' emotional journey,
+                    one of the most memorable and poignant moments
+                  </p>
+                  <p>
+                    City of stars <br />
+                    Are you shining just for me?
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h4 className="musicPageHeader4">A Lovely Night</h4>
+                <div className="soundTrackImg">
+                  <a
+                    href={"https://www.youtube.com/watch?v=_8w9rOpV3gc"}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={ALovelyNight}
+                      alt="A Lovely Night"
+                      className="songImg"
+                      align="left"
+                    />
+                  </a>
+                  Mia (Emma Stone) and Sebastian (Ryan Gosling) share a playful,
+                  flirtatious dance at twilight in Griffith Park. The song
+                  humorously downplays their attraction while showcasing their
+                  growing chemistry. The scene, with its elegant choreography
+                  and stunning backdrop, beautifully captures the film's blend
+                  of classic Hollywood homage and modern romance.
+                </div>
+              </div>
+              <div>
+                <h4 className="musicPageHeader4">Planetarium</h4>
+                <div className="soundTrackImg">
+                  <a
+                    href={"https://www.youtube.com/watch?v=lNFbbWOM5FU"}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={Planetarium}
+                      alt="A Lovely Night"
+                      className="songImg"
+                      align="left"
+                    />
+                  </a>
+                  Mia and Sebastian share a magical moment while dancing among
+                  the stars. Their connection deepens as they sing about their
+                  hopes and dreams, encapsulating the emotional journey of love,
+                  ambition, and the bittersweet realities of pursuing one's
+                  passions. This scene beautifully captures the essence of the
+                  film's themes, leaving a lasting impression on both characters
+                  and audience alike.
+                </div>
+              </div>
+              <div>
+                <h4 className="musicPageHeader4">Epilogue</h4>
+                <div className="soundTrackImg">
+                  <a
+                    href={"https://www.youtube.com/watch?v=Lw0U5rhrhaI"}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={Epilogue}
+                      alt="Epilogue"
+                      className="songImg"
+                      align="left"
+                    />
+                  </a>
+                  Mia visits Sebastian's jazz club years later, sparking a
+                  fantasy sequence imagining their idealized relationship. The
+                  bittersweet montage ends with Mia and Sebastian sharing a
+                  silent, knowing smile, highlighting the film's themes of love,
+                  ambition, and sacrifice.
+                </div>
+              </div>
             </div>
-            <h4 className="musicPageHeader4">A Lovely Night</h4>
-            <div className="soundTrackImg">
-              <a
-                href={"https://www.youtube.com/watch?v=_8w9rOpV3gc"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={ALovelyNight}
-                  alt="A Lovely Night"
-                  className="songImg"
-                />
-              </a>
-              {` A playful, flirtatious, elegant dance at twilight, showcasing
-              their chemistry`}
-            </div>
-            <h4 className="musicPageHeader4">Planetarium</h4>
-            <div className="soundTrackImg">
-              <a
-                href={"https://www.youtube.com/watch?v=lNFbbWOM5FU"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={Planetarium}
-                  alt="A Lovely Night"
-                  className="songImg"
-                />
-              </a>
-              {` Mia and Sebastian share a magical moment while dancing among the
-              stars.`}
-            </div>
-            <h4 className="musicPageHeader4">Epilogue</h4>
-            <div className="soundTrackImg">
-              <a
-                href={"https://www.youtube.com/watch?v=Lw0U5rhrhaI"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={Epilogue} alt="Epilogue" className="songImg" />
-              </a>
-              {` Sparking a fantasy of their ideal relationship encapsulates the
-              love and sacrifice`}
-            </div>
-            <br />
             <br />
             <br />
             <hr className="my-5" />
             <div className="imageBanner">
-              <Card className="explosionGradient">
+              <Card className="explosionGradient defaultMusicCard">
                 <Card.Img variant="top" src={Riders} />
                 <Card.Img variant="top" src={RaidersPoster} />
                 <Card.Body>
@@ -129,7 +168,7 @@ function MusicPage() {
                   </a>
                 </Card.Body>
               </Card>
-              <Card className="defaultCardTheme">
+              <Card className="defaultCardTheme defaultMusicCard">
                 <Card.Img variant="top" src={lilDickyEarth} />
                 <Card.Img variant="top" src={Earth} />
                 <Card.Body>
